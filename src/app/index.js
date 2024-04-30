560d11543473be0c248607f4ca6fa417061d29de
LemuelFreitas <lemuel.zuin.freitas@gmail.com>
2024-04-30 15:40:13 -0300

    CriandoComponentesReactExpo

---
+++ src/app/index.js
@@ -0,0 +1,32 @@
+import { View, Image, Text } from 'react-native';
+import { styles } from '../css/style';
+import Botao from '../components/botao';
+import InputTexto from '../components/inputTexto';
+import { useState } from 'react';
+
+export default function App() {
+  const [valida, setValida] = useState();
+  const atualizaTexto = (newText) =>{
+    console.log(newText);
+  }
+  return (
+    <View style={styles.container}>
+       <Image style={{width:'80%', height:80, marginBottom:30}}
+      resizeMome="contain"
+      source={require('../../assets/IMGFacebook.png')}
+      />
+      <InputTexto label="Digite seu nome" dica="Digite seu nome"/>
+      
+      <InputTexto label="Digite sua senha" dica="Digite sua senha" seguranca={true}/>
+
+      <Botao btn="entrar" cor='#0000FF' src='home' />
+      <View style={styles.cont}>
+        <View style={styles.line} />
+          <Text style={styles.text}>ou</Text>
+        <View style={styles.line} />
+      </View>
+      <Botao btn="cadastrar" cor='#' src='cadastrar' />
+    </View>
+  );
+}
+
---
+++ src/css/botaoStyle.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    button: {
+     borderWidth: 1,
+     borderColor: '#EBECEE',
+     paddingVertical: 20,
+     paddingHorizontal: 40,
+     borderRadius: 5,
+     marginVertical: 10,
+    },
+    buttonPressed: {
+        backgroundColor: '#0056b3'
+    },
+    buttonText: {
+        color: '#000000',
+        fontSize: 16,
+        textAlign: 'center',
+    }
+  });
+  
